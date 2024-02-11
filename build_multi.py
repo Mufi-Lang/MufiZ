ae0ff7430024c53abf6d99870bc3494bb48e407e
Mustafif <mustafif0929@gmail.com>
2024-02-11 14:23:27 -0500

    Add Ruby setup and fpm installation***
    ***Update .gitignore to include fpm.txt***
    
    ***Update build_multi.py to build debian packages for Linux targets***
    
    ***Remove debugging code from common.h

--- build_multi.py
+++ build_multi.py
@@ -3,7 +3,7 @@ import os
 import zipfile
 import json
 
-codename = "voxl"
+version = "0.4.0"
 out_path = "zig-out/bin/"
 windows = f"{out_path}mufiz.exe"
 bin = "zig-out/bin/mufiz"
@@ -17,15 +17,26 @@ for target in targets:
     command = "zig build -Doptimize=ReleaseSafe -Dtarget=" + target
     subprocess.run(command, shell=True, text=True)
     if(target == "x86_64-windows" or target == "aarch64-windows"):
-        windows_zip = f"mufiz_{codename}_{target}.zip"
+        windows_zip = f"mufiz_{version}_{target}.zip"
         with zipfile.ZipFile(out_path+windows_zip, 'w') as wz: 
             wz.write(windows, os.path.basename(windows))
         os.remove(windows)
         print(f"Zipped successfully {windows_zip}")
     else: 
-        zipper = f"mufiz_{codename}_{target}.zip"
+        zipper = f"mufiz_{version}_{target}.zip"
         with zipfile.ZipFile(out_path+zipper, 'w') as z: 
             z.write(bin, os.path.basename(bin))
         os.remove(bin)
         print(f"Zipped successfully {zipper}")
 os.remove(out_path+"mufiz.pdb")
+
+# Build debian packages for Linux targets 
+for target in targets: 
+    if ("x86_64-linux" in target): 
+        command = f"fpm -v {version} -a amd64 -s zip -t deb --prefix /usr/bin -n mufiz zig-out/bin/mufiz_{version}_{target}.zip "
+        subprocess.run(command, shell=True, text=True)
+        print(f"Built debian package for {target}")
+    elif ("aarch64-linux" in target):
+        command = f"fpm -v {version} -a arm64 -s zip -t deb --prefix /usr/bin -n mufiz zig-out/bin/mufiz_{version}_{target}.zip "
+        subprocess.run(command, shell=True, text=True)
+        print(f"Built debian package for {target}")
