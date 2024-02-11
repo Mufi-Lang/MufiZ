e9567ae970aa410cb5343e00e90cd25d779da562
Mustafif <mustafif0929@gmail.com>
2024-02-10 21:00:07 -0500

    Fix build command in main.yml and update codename in release.yml

--- build_multi.py
+++ build_multi.py
@@ -5,16 +5,9 @@ import shutil
 
 codename = "voxl"
 out_path = "zig-out/bin/"
-def rename_file(old_name, new_name):
-    try:
-        shutil.move(old_name, new_name)
-        print(f"File '{old_name}' renamed to '{new_name}' successfully.")
-    except FileNotFoundError:
-        print(f"Error: File '{old_name}' not found.")
-    except Exception as e:
-        print(f"An error occurred: {e}")
-
+windows = f"{out_path}mufiz.exe"
 bin = "zig-out/bin/mufiz"
+
 targets = [
     "aarch64-macos", 
     "x86_64-macos", 
@@ -34,13 +27,15 @@ for target in targets:
     command = "zig build -Doptimize=ReleaseFast -Dtarget=" + target
     subprocess.run(command, shell=True, text=True)
     if(target == "x86_64-windows" or target == "aarch64-windows"):
-        windows =  f'{bin}_{codename}_{target}'+".exe"
-        rename_file(bin+".exe", windows)
         windows_zip = f"mufiz_{codename}_{target}.zip"
         with zipfile.ZipFile(out_path+windows_zip, 'w') as wz: 
             wz.write(windows, os.path.basename(windows))
         os.remove(windows)
         print(f"Zipped successfully {windows_zip}")
     else: 
-        rename_file(bin, f'{bin}_{codename}_{target}')
+        zipper = f"mufiz_{codename}_{target}.zip"
+        with zipfile.ZipFile(out_path+zipper, 'w') as z: 
+            z.write(bin, os.path.basename(bin))
+        os.remove(bin)
+        print(f"Zipped successfully {zipper}")
 os.remove(out_path+"mufiz.pdb")
