795b6355a4e4d92c5305cebee5368b8f71a006e6
Mustafif <mustafif0929@gmail.com>
2024-02-12 20:19:03 -0500

    Refactor fs module to use std.fs.cwd() for creating files

--- src/stdlib/fs.zig
+++ src/stdlib/fs.zig
@@ -4,13 +4,13 @@ const Value = @cImport(@cInclude("value.h")).Value;
 const stdlib_error = @import("../stdlib.zig").stdlib_error;
 const type_check = conv.type_check;
 const GlobalAlloc = @import("../main.zig").GlobalAlloc;
-const fs = std.fs.cwd();
+const fs = std.fs;
 
 // Unable to do string type check
 pub fn create_file(argc: c_int, args: [*c]Value) callconv(.C) Value {
     if (argc != 1) return stdlib_error("Expected 1 argument for create_file()!", .{ .argn = argc });
     const path = conv.as_zstring(args[0]);
-    const file = fs.createFile(path, .{}) catch return conv.bool_val(false);
+    const file = fs.cwd().createFile(path, .{}) catch return conv.bool_val(false);
     defer file.close();
 
     return conv.bool_val(true);
