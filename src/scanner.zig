1c8362eea7b69ea397b3b189dd87a21d105ef73a
Mustafif <mustafif0929@gmail.com>
2024-02-20 11:53:31 -0500

    Update token types in scanner.zig

--- src/scanner.zig
+++ src/scanner.zig
@@ -24,48 +24,50 @@ pub const TokenType = enum(c_int) {
     TOKEN_RIGHT_PAREN = 1,
     TOKEN_LEFT_BRACE = 2,
     TOKEN_RIGHT_BRACE = 3,
-    TOKEN_COMMA = 4,
-    TOKEN_DOT = 5,
-    TOKEN_MINUS = 6,
-    TOKEN_PLUS = 7,
-    TOKEN_SEMICOLON = 8,
-    TOKEN_SLASH = 9,
-    TOKEN_STAR = 10,
+    TOKEN_LEFT_SQUARE = 4,
+    TOKEN_RIGHT_SQUARE = 5,
+    TOKEN_COMMA = 6,
+    TOKEN_DOT = 7,
+    TOKEN_MINUS = 8,
+    TOKEN_PLUS = 9,
+    TOKEN_SEMICOLON = 10,
+    TOKEN_SLASH = 11,
+    TOKEN_STAR = 12,
     // One or more character tokens
-    TOKEN_BANG = 11,
-    TOKEN_BANG_EQUAL = 12,
-    TOKEN_EQUAL = 13,
-    TOKEN_EQUAL_EQUAL = 14,
-    TOKEN_GREATER = 15,
-    TOKEN_GREATER_EQUAL = 16,
-    TOKEN_LESS = 17,
-    TOKEN_LESS_EQUAL = 18,
+    TOKEN_BANG = 13,
+    TOKEN_BANG_EQUAL = 14,
+    TOKEN_EQUAL = 15,
+    TOKEN_EQUAL_EQUAL = 16,
+    TOKEN_GREATER = 17,
+    TOKEN_GREATER_EQUAL = 18,
+    TOKEN_LESS = 19,
+    TOKEN_LESS_EQUAL = 20,
     // Literals
-    TOKEN_IDENTIFIER = 19,
-    TOKEN_STRING = 20,
-    TOKEN_DOUBLE = 21,
-    TOKEN_INT = 22,
+    TOKEN_IDENTIFIER = 21,
+    TOKEN_STRING = 22,
+    TOKEN_DOUBLE = 23,
+    TOKEN_INT = 24,
     // Keywords
-    TOKEN_AND = 23,
-    TOKEN_CLASS = 24,
-    TOKEN_ELSE = 25,
-    TOKEN_FALSE = 26,
-    TOKEN_FOR = 27,
-    TOKEN_FUN = 28,
-    TOKEN_IF = 29,
-    TOKEN_LET = 30,
-    TOKEN_NIL = 31,
-    TOKEN_OR = 32,
-    TOKEN_PRINT = 33,
-    TOKEN_RETURN = 34,
-    TOKEN_SELF = 35,
-    TOKEN_SUPER = 36,
-    TOKEN_TRUE = 37,
-    TOKEN_VAR = 38,
-    TOKEN_WHILE = 39,
-    //Misc
-    TOKEN_ERROR = 40,
-    TOKEN_EOF = 41,
+    TOKEN_AND = 25,
+    TOKEN_CLASS = 26,
+    TOKEN_ELSE = 27,
+    TOKEN_FALSE = 28,
+    TOKEN_FOR = 29,
+    TOKEN_FUN = 30,
+    TOKEN_IF = 31,
+    TOKEN_LET = 32,
+    TOKEN_NIL = 33,
+    TOKEN_OR = 34,
+    TOKEN_PRINT = 35,
+    TOKEN_RETURN = 36,
+    TOKEN_SELF = 37,
+    TOKEN_SUPER = 38,
+    TOKEN_TRUE = 39,
+    TOKEN_VAR = 40,
+    TOKEN_WHILE = 41,
+    // Misc
+    TOKEN_ERROR = 42,
+    TOKEN_EOF = 43,
 };
 
 pub const Token = extern struct {
@@ -162,7 +164,7 @@ pub export fn checkKeyword(arg_start: c_int, arg_length: c_int, arg_rest: [*c]co
     var length = arg_length;
     var rest = arg_rest;
     var @"type" = arg_type;
-    if (@intFromPtr(scanner.current) - @intFromPtr(scanner.start) == start + length and memcmp(@ptrCast(@as([*c]u8, @ptrFromInt(@intFromPtr(scanner.start) + @as(usize, @intCast(start))))), @ptrCast(rest), @intCast(length)) == 0 ) {
+    if (@intFromPtr(scanner.current) - @intFromPtr(scanner.start) == start + length and memcmp(@ptrCast(@as([*c]u8, @ptrFromInt(@intFromPtr(scanner.start) + @as(usize, @intCast(start))))), @ptrCast(rest), @intCast(length)) == 0) {
         return @"type";
     }
     return .TOKEN_IDENTIFIER;
@@ -235,7 +237,7 @@ pub export fn __scanner__number() callconv(.C) Token {
 
 pub export fn __scanner__string() callconv(.C) Token {
     while (peek() != '"' and !isAtEnd()) {
-        if (peek() ==  '\n') {
+        if (peek() == '\n') {
             scanner.line += 1;
         }
         _ = __scanner__advance();
@@ -259,6 +261,8 @@ pub export fn scanToken() Token {
         ')' => return makeToken(.TOKEN_RIGHT_PAREN),
         '{' => return makeToken(.TOKEN_LEFT_BRACE),
         '}' => return makeToken(.TOKEN_RIGHT_BRACE),
+        '[' => return makeToken(.TOKEN_LEFT_SQUARE),
+        ']' => return makeToken(.TOKEN_RIGHT_SQUARE),
         ';' => return makeToken(.TOKEN_SEMICOLON),
         ',' => return makeToken(.TOKEN_COMMA),
         '.' => return makeToken(.TOKEN_DOT),
