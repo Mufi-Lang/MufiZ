2f64aab5d730991dab42ed43bba09ff6af1a1c6e
Mustafif <mustafif0929@gmail.com>
2024-05-09 23:09:47 -0400

    chore: Refactor type checking functions and add helper macros for soft numeric req for builtins

--- src/stdlib/math.zig
+++ src/stdlib/math.zig
@@ -10,23 +10,23 @@ const type_check = conv.type_check;
 
 pub fn ln(argc: c_int, args: [*c]Value) callconv(.C) Value {
     if (argc != 1) return stdlib_error("ln() expects one argument!", .{ .argn = argc });
-    if (!type_check(1, args, 3)) return stdlib_error("ln() expects a Double!", .{ .value_type = conv.what_is(args[0]) });
-    const double = conv.as_double(args[0]);
+    if (!type_check(1, args, 6)) return stdlib_error("ln() expects a Number!", .{ .value_type = conv.what_is(args[0]) });
+    const double = conv.as_num_double(args[0]);
     return conv.double_val(@log(double));
 }
 
 /// log2(double) double
 pub fn log2(argc: c_int, args: [*c]Value) callconv(.C) Value {
     if (argc != 1) return stdlib_error("log2() expects one argument!", .{ .argn = argc });
-    if (!type_check(1, args, 3)) return stdlib_error("log2() expects a Double!", .{ .value_type = conv.what_is(args[0]) });
-    const double = conv.as_double(args[0]);
+    if (!type_check(1, args, 6)) return stdlib_error("log2() expects a Number!", .{ .value_type = conv.what_is(args[0]) });
+    const double = conv.as_num_double(args[0]);
     return conv.double_val(@log2(double));
 }
 /// log10(double) double
 pub fn log10(argc: c_int, args: [*c]Value) callconv(.C) Value {
     if (argc != 1) return stdlib_error("log10() expects one argument!", .{ .argn = argc });
-    if (!type_check(1, args, 3)) return stdlib_error("log10() expects a Double!", .{ .value_type = conv.what_is(args[0]) });
-    const double = conv.as_double(args[0]);
+    if (!type_check(1, args, 6)) return stdlib_error("log10() expects a Number!", .{ .value_type = conv.what_is(args[0]) });
+    const double = conv.as_num_double(args[0]);
     return conv.double_val(@log10(double));
 }
 /// pi() double
@@ -38,59 +38,59 @@ pub fn pi(argc: c_int, args: [*c]Value) callconv(.C) Value {
 
 pub fn exp(argc: c_int, args: [*c]Value) callconv(.C) Value {
     if (argc != 1) return stdlib_error("exp() expects one argument!", .{ .argn = argc });
-    if (!type_check(1, args, 3)) return stdlib_error("exp() expects a Double!", .{ .value_type = conv.what_is(args[0]) });
-    const double = conv.as_double(args[0]);
+    if (!type_check(1, args, 6)) return stdlib_error("exp() expects a Number!", .{ .value_type = conv.what_is(args[0]) });
+    const double = conv.as_num_double(args[0]);
     return conv.double_val(@exp(double));
 }
 
 /// sin(double) double
 pub fn sin(argc: c_int, args: [*c]Value) callconv(.C) Value {
     if (argc != 1) return stdlib_error("sin() expects one argument!", .{ .argn = argc });
-    if (!type_check(1, args, 3)) return stdlib_error("sin() expects a Double!", .{ .value_type = conv.what_is(args[0]) });
-    const double = conv.as_double(args[0]);
+    if (!type_check(1, args, 6)) return stdlib_error("sin() expects a Number!", .{ .value_type = conv.what_is(args[0]) });
+    const double = conv.as_num_double(args[0]);
     return conv.double_val(@sin(double));
 }
 /// cos(double) double
 pub fn cos(argc: c_int, args: [*c]Value) callconv(.C) Value {
     if (argc != 1) return stdlib_error("cos() expects one argument!", .{ .argn = argc });
-    if (!type_check(1, args, 3)) return stdlib_error("cos() expects a Double!", .{ .value_type = conv.what_is(args[0]) });
-    const double = conv.as_double(args[0]);
+    if (!type_check(1, args, 6)) return stdlib_error("cos() expects a Number!", .{ .value_type = conv.what_is(args[0]) });
+    const double = conv.as_num_double(args[0]);
     return conv.double_val(@cos(double));
 }
 /// tan(double) double
 pub fn tan(argc: c_int, args: [*c]Value) callconv(.C) Value {
     if (argc != 1) return stdlib_error("tan() expects one argument!", .{ .argn = argc });
-    if (!type_check(1, args, 3)) return stdlib_error("tan() expects a Double!", .{ .value_type = conv.what_is(args[0]) });
-    const double = conv.as_double(args[0]);
+    if (!type_check(1, args, 6)) return stdlib_error("tan() expects a Number!", .{ .value_type = conv.what_is(args[0]) });
+    const double = conv.as_num_double(args[0]);
     return conv.double_val(@tan(double));
 }
 /// asin(double) double
 pub fn asin(argc: c_int, args: [*c]Value) callconv(.C) Value {
     if (argc != 1) return stdlib_error("asin() expects one argument!", .{ .argn = argc });
-    if (!type_check(1, args, 3)) return stdlib_error("asin() expects a Double!", .{ .value_type = conv.what_is(args[0]) });
-    const double = conv.as_double(args[0]);
+    if (!type_check(1, args, 6)) return stdlib_error("asin() expects a Number!", .{ .value_type = conv.what_is(args[0]) });
+    const double = conv.as_num_double(args[0]);
     return conv.double_val(std.math.asin(double));
 }
 /// acos(double) double
 pub fn acos(argc: c_int, args: [*c]Value) callconv(.C) Value {
     if (argc != 1) return stdlib_error("acos() expects one argument!", .{ .argn = argc });
-    if (!type_check(1, args, 3)) return stdlib_error("acos() expects a Double!", .{ .value_type = conv.what_is(args[0]) });
-    const double = conv.as_double(args[0]);
+    if (!type_check(1, args, 6)) return stdlib_error("acos() expects a Number!", .{ .value_type = conv.what_is(args[0]) });
+    const double = conv.as_num_double(args[0]);
     return conv.double_val(std.math.acos(double));
 }
 /// atan(double) double
 pub fn atan(argc: c_int, args: [*c]Value) callconv(.C) Value {
     if (argc != 1) return stdlib_error("atan() expects one argument!", .{ .argn = argc });
-    if (!type_check(1, args, 3)) return stdlib_error("atan() expects a Double!", .{ .value_type = conv.what_is(args[0]) });
-    const double = conv.as_double(args[0]);
+    if (!type_check(1, args, 6)) return stdlib_error("atan() expects a Number!", .{ .value_type = conv.what_is(args[0]) });
+    const double = conv.as_num_double(args[0]);
     return conv.double_val(std.math.atan(double));
 }
 
 pub fn complex(argc: c_int, args: [*c]Value) callconv(.C) Value {
     if (argc != 2) return stdlib_error("complex() expects two arguments!", .{ .argn = argc });
-    if (!type_check(2, args, 3)) return stdlib_error("complex() expects 2 Double!", .{ .value_type = conv.what_is(args[0]) });
-    const r = conv.as_double(args[0]);
-    const i = conv.as_double(args[1]);
+    if (!type_check(2, args, 6)) return stdlib_error("complex() expects 2 Number!", .{ .value_type = conv.what_is(args[0]) });
+    const r = conv.as_num_double(args[0]);
+    const i = conv.as_num_double(args[1]);
     return conv.complex_val(r, i);
 }
 
@@ -102,11 +102,11 @@ pub fn abs(argc: c_int, args: [*c]Value) callconv(.C) Value {
             return conv.double_val(@sqrt(c.r * c.r + c.i * c.i));
         },
         conv.VAL_DOUBLE => {
-            const d = conv.as_double(args[0]);
+            const d = conv.as_num_double(args[0]);
             return conv.double_val(@abs(d));
         },
         conv.VAL_INT => {
-            const i = conv.as_int(args[0]);
+            const i = conv.as_num_int(args[0]);
             return conv.int_val(@intCast(@abs(i)));
         },
         else => return stdlib_error("abs() expects a Numeric Type!", .{ .value_type = conv.what_is(args[0]) }),
@@ -149,52 +149,52 @@ pub fn randn(argc: c_int, args: [*c]Value) callconv(.C) Value {
 
 pub fn pow(argc: c_int, args: [*c]Value) callconv(.C) Value {
     if (argc != 2) return stdlib_error("pow() expects two arguments!", .{ .argn = argc });
-    if (!type_check(2, args, 3)) return stdlib_error("pow() expects 2 Double!", .{ .value_type = conv.what_is(args[0]) });
-    const base = conv.as_double(args[0]);
-    const exponent = conv.as_double(args[1]);
+    if (!type_check(2, args, 6)) return stdlib_error("pow() expects 2 Number!", .{ .value_type = conv.what_is(args[0]) });
+    const base = conv.as_num_double(args[0]);
+    const exponent = conv.as_num_double(args[1]);
     return conv.double_val(std.math.pow(f64, base, exponent));
 }
 
 pub fn sqrt(argc: c_int, args: [*c]Value) callconv(.C) Value {
     if (argc != 1) return stdlib_error("sqrt() expects one argument!", .{ .argn = argc });
-    if (!type_check(1, args, 3)) return stdlib_error("sqrt() expects a Double!", .{ .value_type = conv.what_is(args[0]) });
-    const double = conv.as_double(args[0]);
+    if (!type_check(1, args, 6)) return stdlib_error("sqrt() expects a Number!", .{ .value_type = conv.what_is(args[0]) });
+    const double = conv.as_num_double(args[0]);
     return conv.double_val(@sqrt(double));
 }
 
 pub fn ceil(argc: c_int, args: [*c]Value) callconv(.C) Value {
     if (argc != 1) return stdlib_error("ceil() expects one argument!", .{ .argn = argc });
-    if (!type_check(1, args, 3)) return stdlib_error("ceil() expects a Double!", .{ .value_type = conv.what_is(args[0]) });
-    const double = conv.as_double(args[0]);
+    if (!type_check(1, args, 6)) return stdlib_error("ceil() expects a Number!", .{ .value_type = conv.what_is(args[0]) });
+    const double = conv.as_num_double(args[0]);
     return conv.int_val(@intFromFloat(@ceil(double)));
 }
 
 pub fn floor(argc: c_int, args: [*c]Value) callconv(.C) Value {
     if (argc != 1) return stdlib_error("floor() expects one argument!", .{ .argn = argc });
-    if (!type_check(1, args, 3)) return stdlib_error("floor() expects a Double!", .{ .value_type = conv.what_is(args[0]) });
-    const double = conv.as_double(args[0]);
+    if (!type_check(1, args, 6)) return stdlib_error("floor() expects a Number!", .{ .value_type = conv.what_is(args[0]) });
+    const double = conv.as_num_double(args[0]);
     return conv.int_val(@intFromFloat(@floor(double)));
 }
 
 pub fn round(argc: c_int, args: [*c]Value) callconv(.C) Value {
     if (argc != 1) return stdlib_error("round() expects one argument!", .{ .argn = argc });
-    if (!type_check(1, args, 3)) return stdlib_error("round() expects a Double!", .{ .value_type = conv.what_is(args[0]) });
-    const double = conv.as_double(args[0]);
+    if (!type_check(1, args, 6)) return stdlib_error("round() expects a Number!", .{ .value_type = conv.what_is(args[0]) });
+    const double = conv.as_num_double(args[0]);
     return conv.int_val(@intFromFloat(@round(double)));
 }
 
 pub fn max(argc: c_int, args: [*c]Value) callconv(.C) Value {
     if (argc != 2) return stdlib_error("max() expects two arguments!", .{ .argn = argc });
-    if (!type_check(2, args, 3)) return stdlib_error("max() expects 2 Double!", .{ .value_type = conv.what_is(args[0]) });
-    const a = conv.as_double(args[0]);
-    const b = conv.as_double(args[1]);
+    if (!type_check(2, args, 6)) return stdlib_error("max() expects 2 Number!", .{ .value_type = conv.what_is(args[0]) });
+    const a = conv.as_num_double(args[0]);
+    const b = conv.as_num_double(args[1]);
     return conv.double_val(@max(a, b));
 }
 
 pub fn min(argc: c_int, args: [*c]Value) callconv(.C) Value {
     if (argc != 2) return stdlib_error("min() expects two arguments!", .{ .argn = argc });
-    if (!type_check(2, args, 3)) return stdlib_error("min() expects 2 Double!", .{ .value_type = conv.what_is(args[0]) });
-    const a = conv.as_double(args[0]);
-    const b = conv.as_double(args[1]);
+    if (!type_check(2, args, 6)) return stdlib_error("min() expects 2 Number!", .{ .value_type = conv.what_is(args[0]) });
+    const a = conv.as_num_double(args[0]);
+    const b = conv.as_num_double(args[1]);
     return conv.double_val(@min(a, b));
 }
